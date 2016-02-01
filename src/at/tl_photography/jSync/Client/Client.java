d7792ae8cd165b860d062b1f0f4b08c9b27b625d
Herschdorfer <herschdorfer@gmail-com>
2016-02-01 14:09:46 +0100

    init

---
+++ src/at/tl_photography/jSync/Client/Client.java
@@ -0,0 +1,10 @@
+package at.tl_photography.jSync.Client;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/at/tl_photography/jSync/Server/Server.java
@@ -0,0 +1,10 @@
+package at.tl_photography.jSync.Server;
+
+public class Server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
